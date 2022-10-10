210b3250d283bd8259bb262e6c2de99530124eca
Aditya469 <aditya.kolluri0@gmail.com>
2022-10-10 10:46:23 +0100

    fix for systemConfig

--- timelogger/pages/overview_client.php
+++ timelogger/pages/overview_client.php
@@ -1,11 +1,13 @@
 <?php 
 
 require "client_config.php";
-// require "../scripts/server/systemConfig.php";
 
-function showQuantityComplete(){
-	return true;
-}
+$systemConfig_path = $_SERVER['DOCUMENT_ROOT'] . "/scripts/server/systemConfig.php";
+require $systemConfig_path;
+
+// function showQuantityComplete(){
+// 	return true;
+// }
 
 if($showChargeDisplayElements)
 {
@@ -34,13 +36,10 @@ else
 <html>
     <head>
 		<!-- Copyright 2022 DigitME2
-
 		Licensed under the Apache License, Version 2.0 (the "License");
 		you may not use this file except in compliance with the License.
 		You may obtain a copy of the License at
-
 			http://www.apache.org/licenses/LICENSE-2.0
-
 		Unless required by applicable law or agreed to in writing, software
 		distributed under the License is distributed on an "AS IS" BASIS,
 		WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
