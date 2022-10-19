119471a6e7318b85bf79e184a9a18372c9292138
Aditya469 <aditya.kolluri0@gmail.com>
2022-10-19 13:42:41 +0100

    overview page bug fixed

--- work_tracking_db_setup.sql
+++ work_tracking_db_setup.sql
@@ -730,6 +730,8 @@ BEGIN
     CREATE TEMPORARY TABLE openTimes (jobId VARCHAR(20), openDuration INT, openOvertimeDuration INT);
     CREATE TEMPORARY TABLE selectedJobIds (counter INT PRIMARY KEY AUTO_INCREMENT, jobId VARCHAR(20));
 	CREATE TEMPORARY TABLE closedRecords (jobId VARCHAR(20), closedDuration INT, closedOvertimeDuration INT, quantityComplete INT);
+	CREATE TEMPORARY TABLE totalDurations (jobId VARCHAR(20), totalWorkedDuration INT, totalOvertimeDuration INT);
+	CREATE TEMPORARY TABLE quantities(jobId VARCHAR(20), quantityComplete INT);
 
 
 	-- Construct a query to select the job IDs meeting the required selection criteria (completed or not, date range, etc)...
@@ -867,9 +869,9 @@ BEGIN
     -- test
 
 
-	--  SELECT * FROM openTimes;
-	--  SELECT * FROM closedRecords;
-	--  SELECT * FROM recordQuantityComplete;
+	-- SELECT * FROM openTimes;
+	-- SELECT * FROM closedRecords;
+--	SELECT * FROM recordQuantityComplete;
 	-- -- SELECT ExcludeUnworkedJobs;
 
 	IF LimitDateTimeWorkedRange IS TRUE AND ExcludeUnworkedJobs IS TRUE THEN
@@ -890,6 +892,11 @@ BEGIN
     SELECT jobId, 0, 0, 0 FROM selectedJobIds;
     
     CREATE INDEX idx_closedRecords_jobIds ON closedRecords(jobId);
+    
+    
+--    SELECT * FROM openTimes;
+--	SELECT * FROM closedRecords;
+    
 
 	-- ...appending the relevant selection options...
 	IF UseSearchKey IS TRUE THEN
@@ -898,7 +905,16 @@ BEGIN
 		-- this is set to " WHERE ", then changed to " AND " after the first condition is set.
 		SET @conditionPrecederTerm = " AND "; 
 	END IF;	
+	
+	INSERT INTO totalDurations (jobId, totalWorkedDuration, totalOvertimeDuration) SELECT jobId, SUM(openDuration), SUM(openOvertimeDuration) FROM openTimes GROUP BY jobId;
+	INSERT INTO totalDurations (jobId, totalWorkedDuration, totalOvertimeDuration) SELECT jobId, SUM(closedDuration), SUM(closedOvertimeDuration) FROM closedRecords GROUP BY jobId;
+	
+--	SELECT * FROM totalDurations;
+--	SELECT jobs.jobId, SUM(totalDurations.totalWorkedDuration) FROM jobs LEFT JOIN totalDurations on jobs.jobId = totalDurations.jobId;
     
+    INSERT INTO quantities(jobId, quantityComplete) SELECT jobId, SUM(quantityComplete) FROM closedRecords GROUP BY jobId;
+--    SELECT "Inserted into quantities";
+--    SELECT * FROM quantities;
     
     
     -- Create and run the final query to select the data from the timeLog and combine
@@ -910,10 +926,10 @@ BEGIN
     description,
     currentStatus,
     recordAdded,
-    SUM(closedRecords.closedDuration) + SUM(openDuration) AS totalWorkedDuration,
-    SUM(closedRecords.closedOvertimeDuration) + SUM(openOvertimeDuration) AS totalOvertimeDuration,
-	SUM(closedRecords.quantityComplete) AS quantityComplete,
-    LEAST((expectedDuration/(SUM(closedRecords.closedDuration) + SUM(openDuration))),1) AS efficiency,
+    SUM(totalDurations.totalWorkedDuration) AS totalWorkedDuration,
+    SUM(totalDurations.totalOvertimeDuration) AS totalOvertimeDuration,
+	quantities.quantityComplete AS quantityComplete,
+    LEAST((expectedDuration/(SUM(totalDurations.totalWorkedDuration))),1) AS efficiency,
     expectedDuration,
     routeCurrentStageName,
     priority,
@@ -927,10 +943,11 @@ BEGIN
    	stageOutstandingUnits,
    	customerName,
    	notes
-   	FROM jobs LEFT JOIN openTimes ON jobs.jobId = openTimes.jobId
-	LEFT JOIN closedRecords ON jobs.jobId = closedRecords.jobId
-   	WHERE jobs.jobId IN (SELECT jobId FROM selectedJobIds ORDER BY counter ASC)
-   	GROUP BY jobs.jobId ";
+   	FROM jobs
+   	LEFT JOIN totalDurations ON jobs.jobId = totalDurations.jobId
+	LEFT JOIN quantities ON jobs.jobId = quantities.jobId
+   	WHERE jobs.jobId IN (SELECT jobId FROM selectedJobIds ORDER BY counter ASC) 
+ 	GROUP BY jobs.jobId ";
 
 
 	
@@ -953,7 +970,7 @@ BEGIN
 	IF SubOrderByPriority IS TRUE THEN
 		SET @finalSelectorQuery = CONCAT(@finalSelectorQuery, ", priority DESC ");
 	END IF;
-    
+	
 	PREPARE jobSelectionStmt FROM @finalSelectorQuery;
 	EXECUTE jobSelectionStmt;
 	DEALLOCATE PREPARE jobSelectionStmt;
@@ -961,6 +978,7 @@ BEGIN
     DROP TABLE openTimes;
 	DROP TABLE closedRecords;
     DROP TABLE selectedJobIds;
+
 END$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `GetStoppagesLog` (IN `JobId` VARCHAR(20))  MODIFIES SQL DATA
