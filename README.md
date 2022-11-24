9b06d1ffdc24ade4c97268bf74d2f5322de915dc
Aditya469 <aditya.kolluri0@gmail.com>
2022-11-24 11:51:21 +0000

    clean

--- README.md
+++
@@ -1,3 +0,0 @@
-# DigitME2 Process Time Tracker
-
-Setup script requires xampp-linux-x64-7.2.12-0-installer.run in root directory
--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/opt/lampp/xampp start
-sleep 20
-mysql -u root -e "SET GLOBAL event_scheduler = ON" -S /opt/lampp/var/mysql/mysql.sock
-
-# To keep the docker container alive
-sleep infinity
